955ea6ea5f401d1c0ee0468be02185ce37a4c723
aBudreikaite <budreikaiteausteja@gmail.com>
2023-06-22 20:00:22 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict"
+
+alert("Press the button to learn something new!")
+
+var tabs = document.getElementsByClassName("tab")
+var buttons = document.getElementsByTagName("button")
+
+function changeTab (event, name){
+    for(var tab of tabs){
+        tab.classList.remove("block")
+
+        document.getElementById(name).classList.add("block")
+
+        for(var button of buttons){
+            button.classList.remove("selected")
+            } 
+            event.currentTarget.classList.add("selected")
+    }
+}
